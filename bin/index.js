a71afe89b473f9d44f117d26047a08df65b41eb8
zero <zl18180276692@outlook.com>
2023-11-22 12:40:51 +0800

    feat: first update

--- bin/index.js
+++ bin/index.js
@@ -22,12 +22,26 @@ program.version("0.0.1").name("crisp-log").description(`
 program
   .command("run [type] [message]")
   .description("开始生成提交信息")
-  .action(async (type, message) => {
+  .action(async (p1, p2) => {
     let _type, _message;
-    if (type) {
-      _type = type;
+    if (p1 && p2) {
+      _type = p1;
+      _message = p2;
+      toLog(_type, _message);
+    } else if (p1 && !p2) {
+      _message = p1;
+      exec("git symbolic-ref --short -q HEAD", (err, stdout, stderr) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        const branch = stdout.trim();
+        // 检测分支名是否包含 type
+        _type = Object.keys(typeMap).find((key) => branch.includes(key));
+        toLog(_type, _message);
+      });
     } else {
-      const answers = await inquirer.prompt([
+      const type_answer = await inquirer.prompt([
         {
           type: "list",
           name: "type",
@@ -35,22 +49,27 @@ program
           choices: Object.keys(typeMap),
         },
       ]);
-      _type = answers.type;
-    }
-    if (message) {
-      _message = message;
-    } else {
-      const answers = await inquirer.prompt([
+      _type = type_answer.type;
+      const message_answer = await inquirer.prompt([
         {
           type: "input",
           name: "message",
           message: "请输入提交信息:",
         },
       ]);
-      _message = answers.message;
+      _message = message_answer.message;
+      toLog(_type, _message);
     }
-    const commitMessage = `${typeMap[_type]|| _type} : ${_message}`;
-    console.log(`\n${commitMessage}\n`);
+    function toLog(type, message) {
+      const commitMessage = `${typeMap[type] || type}: ${message}`;
+      exec(`git add . && git commit -m "${commitMessage}"`, (err, stdout, stderr) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(stdout);
+      });
+    }
   });
 
 program.parse(process.argv);
