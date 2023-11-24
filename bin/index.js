3fbb7fc4a160a4255319864618657c3ecb012334
qiancheng.zhao <qiancheng.zhao@alo7.com>
2023-11-24 09:29:30 +0800

    feat: update

--- bin/index.js
+++ bin/index.js
@@ -32,7 +32,6 @@ program
       _type = p1;
       _message = p2;
       toLog(_type, _message);
-      (option.push || option.u) && push();
     } else if (p1 && !p2) {
       _message = p1;
       exec("git symbolic-ref --short -q HEAD", async (err, stdout, stderr) => {
@@ -56,7 +55,6 @@ program
           _type = type_answer.type;
         }
         toLog(_type, _message);
-        (option.push || option.u) && push();
       });
     } else {
       const type_answer = await inquirer.prompt([
@@ -77,7 +75,6 @@ program
       ]);
       _message = message_answer.message;
       toLog(_type, _message);
-      (option.push || option.u) && push();
     }
     function toLog(type, message) {
       const commitMessage = `${typeMap[type] || type}: ${message}`;
@@ -90,7 +87,10 @@ program
             console.log(stdout, stderr);
             return;
           }
-          console.log(stdout);
+          setTimeout(() => {
+            console.log("开始push");
+            (option.push || option.u) && push();
+          }, 5000);
         }
       );
     }
